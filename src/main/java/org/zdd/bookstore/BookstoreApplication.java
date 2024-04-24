160d56118a1a91f2877607e3eac323372aa7d8b0
XiaoZhiqiang <501088355@qq.com>
2024-04-24 10:52:37 +0800

    修改为jar 打包 一键启动 完成

--- src/main/java/org/zdd/bookstore/BookstoreApplication.java
+++ src/main/java/org/zdd/bookstore/BookstoreApplication.java
@@ -3,7 +3,7 @@ package org.zdd.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -13,7 +13,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableCaching//开启缓存
 @MapperScan("org.zdd.bookstore.model.dao")
 @EnableAsync
-public class BookstoreApplication extends SpringBootServletInitializer{
+public class BookstoreApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
